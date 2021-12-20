911cf0a6fb96005a016eadf2a33426521fa634da
ike <40803799+ikmnjrd@users.noreply.github.com>
2021-12-21 01:08:26 +0900

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# セットアップ
+## プロジェクトをクローン
+`# git clone git@github.com:ikmnjrd/react-ssr-knative-boilerplate.git`
+
+`# rm -rf .git/`
+
+`# npm install`
+
+
+## コマンド
+`# npm run build`
---
+++ build.ts
@@ -0,0 +1,30 @@
+const { argv } = require('process')
+const { build } = require('esbuild')
+const path = require('path')
+
+const options = {
+  define: { 'process.env.NODE_ENV': process.env.NODE_ENV },
+  entryPoints: [path.resolve(__dirname, 'src/App.jsx')],
+  minify: argv[2] === 'production',
+  bundle: true,
+  target: 'es2016',
+  platform: 'browser',
+  outdir: path.resolve(__dirname, 'public'),
+  tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+  watch: {
+    onRebuild(err, result) {
+      console.log(JSON.stringify(err?.errors));
+      console.log(JSON.stringify(result?.warnings));
+    },
+  },
+}
+
+build(options)
+.then(() => {
+  console.log("===========================================");
+  console.log(`${new Date().toLocaleString()}: watching...`);
+})
+.catch(err => {
+  process.stderr.write(err.stderr)
+  process.exit(1)
+})
